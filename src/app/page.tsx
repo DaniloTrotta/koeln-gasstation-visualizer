490080854966f42fed7776bf53e25fac3302576b
DaniloTrotta <104756510+DaniloTrotta@users.noreply.github.com>
2025-03-23 15:47:07 +0100

    ✏️  Add desription

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,19 @@ export default async function Home(props: PageParams) {
 				Eine Übersicht aller Tankstellen in Köln mit Filtermöglichkeiten nach
 				Straßennamen und Sortierung.
 			</p>
+			<p className="text-muted-foreground mb-6">
+				Daten werden in eine PostgreSQL Datenbank (NeonDB) importiert bei aufruf
+				der <code className="bg-muted px-1 rounded-md">api/import-data</code>{" "}
+				route.
+				<br />
+				Die Daten werden dann von der Datenbank abgerufen und angezeigt.
+				<br />
+				Es können neue Tankstellen hinzugefügt oder gelöscht werden.
+			</p>
+			<p className="text-muted-foreground mb-6">
+				<span className="font-bold text-primary">Tech-Stack:</span> Next.js,
+				Shadcn, NeonDB, Drizzle, Zod, Nuqs, ZSA
+			</p>
 			<Suspense fallback={<div>Lade...</div>}>
 				<GasStationView searchParams={parsedSearchParams} />
 			</Suspense>
